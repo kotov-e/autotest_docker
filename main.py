47cfc2331d7b40faf01fccbcf98a828eed22be38
Eugene Kotov <eug01001@gmail.com>
2026-01-02 12:37:39 +0300

    add autotest

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 
+import pytest
+from playwright.sync_api import Page, expect, sync_playwright
 
-print("Hello World!")
+
+def test_wiki(page):
+    page.goto('https://www.wikipedia.org/')
+    page.get_by_role('link', name='Русский').click()
+    expect(page.get_by_text('Добро пожаловать в Википедию')).to_be_visible()
+
+def test_wiki2(page: Page):
+    page.goto('https://www.wikipedia.org/')
+    page.locator("#js-link-box-ru").click()
+    expect(page.locator(".main-top-header.mw-html-heading")).to_contain_text("Добро пожаловать в")
+
+def test_wiki3(page: Page):
+    page.goto('https://www.wikipedia.org/')
+    page.locator("#js-link-box-ru").click()
+    page.get_by_role('link', name='Википедию').click()
+    expect(page.locator(".firstHeading.mw-first-heading")).to_contain_text("Википедия")
