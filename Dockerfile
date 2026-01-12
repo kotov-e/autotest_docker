ec5e7cef3e973a08edfc2f284695301cfc4519f0
Eugene Kotov <eug01001@gmail.com>
2026-01-12 13:48:29 +0300

    1201

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,7 @@
-FROM
+FROM python
+COPY requirements.txt .
+COPY main.py .
+RUN pip install --no-cache -r requirements.txt
+RUN playwright install --with-deps chromium
+RUN mkdir allure-results
+CMD ["pytest", "main.py", "--alluredir=allure-results"]
--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 
 import pytest
 from playwright.sync_api import Page, expect, sync_playwright
+import allure
 
-
+@allure.title("Test 1")
 def test_wiki(page):
-    page.goto('https://www.wikipedia.org/')
+    with allure.step("Goto https://www.wikipedia.org/"):
+        page.goto('https://www.wikipedia.org/')
     page.get_by_role('link', name='Русский').click()
     expect(page.get_by_text('Добро пожаловать в Википедию')).to_be_visible()
 
+@allure.title("Test 2")
 def test_wiki2(page: Page):
-    page.goto('https://www.wikipedia.org/')
+    with allure.step("Goto https://www.wikipedia.org/"):
+        page.goto('https://www.wikipedia.org/')
     page.locator("#js-link-box-ru").click()
     expect(page.locator(".main-top-header.mw-html-heading")).to_contain_text("Добро пожаловать в")
 
+@allure.title("Test 3")
 def test_wiki3(page: Page):
-    page.goto('https://www.wikipedia.org/')
+    with allure.step("Goto https://www.wikipedia.org/"):
+        page.goto('https://www.wikipedia.org/')
     page.locator("#js-link-box-ru").click()
     page.get_by_role('link', name='Википедию').click()
     expect(page.locator(".firstHeading.mw-first-heading")).to_contain_text("Википедия")
